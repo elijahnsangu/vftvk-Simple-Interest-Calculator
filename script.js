7157cfa89501426d4892967fd8d2e6e61dd212e6
nsangu <elijahnsangu@gmail.com>
2021-04-11 12:55:29 +0200

    final simple interest app

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+
+
+function compute() {
+    event.preventDefault();
+    const amount = parseFloat(document.getElementById("amount").value);
+    const rate = parseFloat(document.getElementById("rangeInput").value);
+    const timePeriod = parseFloat(document.getElementById("years").value);
+    let year = 2021 + timePeriod;
+
+ 
     
+    
+    
+
+    let simpleInt = amount * rate * timePeriod;
+    let total = ( amount + simpleInt).toFixed(2);
+
+    document.getElementById("result").innerHTML =
+    `If you deposit <mark>${amount}</mark>,<br>
+    at an interest rate of <mark>${rate}%</mark>.<br>
+    You will receive an amount of <mark>${total}</mark>,
+    in the year <mark>${year}</mark>.`;
 }
-        
+
