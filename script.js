444b858a1e5d257d597705550693ffff8786b9ef
nsangu <elijahnsangu@gmail.com>
2021-04-11 13:27:10 +0200

    revised simple interest calc

--- script.js
+++ script.js
@@ -6,13 +6,7 @@ function compute() {
     const rate = parseFloat(document.getElementById("rangeInput").value);
     const timePeriod = parseFloat(document.getElementById("years").value);
     let year = 2021 + timePeriod;
-
- 
-    
-    
-    
-
-    let simpleInt = amount * rate * timePeriod;
+    let simpleInt = (amount * rate * timePeriod)/100;
     let total = ( amount + simpleInt).toFixed(2);
 
     document.getElementById("result").innerHTML =
@@ -22,3 +16,16 @@ function compute() {
     in the year <mark>${year}</mark>.`;
 }
 
+function checkdata(){
+    //create references to the input elements we wish to validate
+    let principle = document.getElementById("amount");
+
+    //Check if principle field is empty
+    if((principle.value == "") || (principle.value < 1)){
+          alert("Please enter a positive number");
+          principle.focus();
+          return false;
+    }
+
+    return true;
+}
